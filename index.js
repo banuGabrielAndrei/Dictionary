7e2455cfe1c1224122964e1c91b20a8eca768116
banuGabrielAndrei <banu.gabrielandrei@gmail.com>
2023-06-05 00:04:04 +0300

    Dictionary basic

---
+++ index.js
@@ -0,0 +1,16 @@
+let dictionaryWords = [];
+
+function addWords() {
+    if (!dictionaryWords.includes(document.getElementById("wordsIn").value)) {
+        dictionaryWords.push(document.getElementById("wordsIn").value);
+    }
+}
+
+function checkWord() {
+    let userMessage = document.getElementById("messageForUser");
+    if (dictionaryWords.includes(document.getElementById("searchedWord").value)) {
+        userMessage.textContent = "The word" + " " + document.getElementById("searchedWord").value + " " + "exists in the Dictionary";
+    } else {
+        userMessage.textContent = "The word" + " " + document.getElementById("searchedWord").value + " " + "cannot be found in the Dictionary";
+    }
+}
